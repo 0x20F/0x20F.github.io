2f0f50268b1fd09400e5ffc13f93fa99658aed27
Poly <alexhexan22@gmail.com>
2019-01-26 13:49:17 +0100

    Still testing

--- feed/scripts/main.js
+++ feed/scripts/main.js
@@ -2,11 +2,6 @@ var user_info_url = "https://artstation.com/users/username/quick.json";
 
 var usernames = Array();
 
-$.ajax({
-    url: "../data/collectors.json",
-    type: "GET",
-    crossDomain: true,
-    dataType: "json"
-}).done(function(data) {
+$.getJSON("../data/collectors.json", function(data) {
     console.log("MAGIC");
 });
