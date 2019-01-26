457e33dbd85a01741f13f1a5a3a3738de92122ce
Poly <alexhexan22@gmail.com>
2019-01-26 14:06:07 +0100

    Changed my mind

--- feed/scripts/main.js
+++
@@ -1,7 +0,0 @@
-var user_info_url = "https://artstation.com/users/username/quick.json";
-
-var usernames = Array();
-
-$.getJSON("../data/collectors.json", function(data) {
-    console.log("MAGIC");
-});
